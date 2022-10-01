f734a97ed97f71fcb9998a5024f25a55317449ce
Minnovate <minh@digivision.vn>
2022-10-01 23:20:04 +0700

    0.1.2 fix a small -y issue in bash file

--- centos/install-docker-kube.sh
+++ centos/install-docker-kube.sh
@@ -62,7 +62,7 @@ sudo yum install -y yum-utils
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo systemctl start docker
 
 # Install containerd
